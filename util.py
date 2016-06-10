79e7e3d335caa93fb184bcaf72c05ade42b6746f
ruiwu1990 <rui@cse.unr.edu>
2016-06-10 12:05:10 -0700

    add some apis for chase and andy

--- util.py
+++ util.py
@@ -33,6 +33,74 @@ def fire_out_file_processing(fire_out_file):
                  'max_val':max_val,\
                  'notsetfire_val':notsetfire_val,\
                  'fire_data':fire_data}
+    fp.close()
+    return json.dumps(json_dict)
+
+def get_fire_meta_data(fire_out_file):
+    '''
+    this function returns the meta data
+    '''
+    # I am kind of hard coded here to grab meta data
+    # without using pandas, coz jessie file is not quit csv style
+    fp = open(fire_out_file)
+    # get let top lat, and remove \n in the end of string
+    let_top_lat = fp.readline().strip().split(':')[1]
+    # get other metadata
+    left_top_long = fp.readline().strip().split(':')[1]
+    right_bottom_lat = fp.readline().strip().split(':')[1]
+    right_bottom_long = fp.readline().strip().split(':')[1]
+    num_rows = fp.readline().strip().split(':')[1]
+    num_cols = fp.readline().strip().split(':')[1]
+    max_val = fp.readline().strip().split(':')[1]
+    notsetfire_val = fp.readline().strip().split(':')[1]
+
+    json_dict = {'let_top_lat':let_top_lat,\
+                 'left_top_long':left_top_long,\
+                 'right_bottom_lat':right_bottom_lat,\
+                 'right_bottom_long':right_bottom_long,\
+                 'num_rows':num_rows,\
+                 'num_cols':num_cols,\
+                 'max_val':max_val,\
+                 'notsetfire_val':notsetfire_val}
+    fp.close()
+    return json.dumps(json_dict)
+
+def get_fire_data_by_timestep(fire_out_file,start_time,end_time):
+    '''
+    start_time and end_time should be int
+    '''
+    fp = open(fire_out_file)
+    # get let top lat, and remove \n in the end of string
+    let_top_lat = fp.readline().strip().split(':')[1]
+    # get other metadata
+    left_top_long = fp.readline().strip().split(':')[1]
+    right_bottom_lat = fp.readline().strip().split(':')[1]
+    right_bottom_long = fp.readline().strip().split(':')[1]
+    num_rows = fp.readline().strip().split(':')[1]
+    num_cols = fp.readline().strip().split(':')[1]
+    max_val = fp.readline().strip().split(':')[1]
+    notsetfire_val = fp.readline().strip().split(':')[1]
+    fire_data = [[[]]*int(num_rows)]*(end_time-start_time+1)
+    count = 0
+    for line in fp:
+        temp_list = line.strip().split(',')
+        # need to pop the last element, coz each row of the output file 
+        # is like this '...,', so the last element will be '' after split(',')
+        temp_list.pop()
+        for i in range(len(fire_data)):
+            temp_item = []
+            for m in temp_list:
+                if m<=(i+start_time):
+                    # 1 means on fire
+                    temp_item.append(1)
+                else:
+                    # 0  means not on fire
+                    temp_item.append(0)            
+            #fire_data[i].append(temp_item)
+            fire_data[i][count] = temp_item
+        count = count + 1
+    json_dict = {'fire_data':fire_data}
+    fp.close()
     return json.dumps(json_dict)
 
 # this function is used to get possible vegtation types
@@ -62,6 +130,7 @@ def get_veg_types(veg_out_file):
     result_list = [int(m) for m in result_list]
     # sort
     result_list.sort()
+    file_handle.close()
     return [meta_data,result_list]
 
 # TODO combine this fun with get_veg_types
--- views.py
+++ views.py
@@ -11,9 +11,30 @@ def obtain_fire_data():
     output_file = app_path + '/static/data/output_test.csv'
     return util.fire_out_file_processing(output_file)
 
+@app.route('/api/fire_data/metadata')
+def obtain_fire_meta_data():
+    # hard coded for now, to get the choose the input file
+    output_file = app_path + '/static/data/output_test.csv'
+    return util.get_fire_meta_data(output_file)
+
+@app.route('/api/fire_frame_data/<start>/<end>')
+def obtain_fire_frame_data(start='',end=''):
+    # hard coded for now, to get the choose the input file
+    output_file = app_path + '/static/data/output_test.csv'
+    return util.get_fire_data_by_timestep(output_file,int(start),int(end))
+
 @app.route('/')
 def index_page():
     return render_template("index.html")
+
+# @app.route('/api/onfire_cell_json/<timestep>')
+# def api_onfire_json():
+# 	'''
+# 	this is an api to return onfire cell
+# 	the json format is like this
+# 	{'onfire':[[col0,row0],[col1,row1],[col2,row2]...]}
+# 	'''
+#     return render_template("index.html")    
 
 @app.route('/display_veg')
 def obtain_veg_data():
